ae6f682849410fb2039fbebb8962f8f38a46ed0b
surferwat <wmiura@gmail.com>
2021-07-05 17:12:32 -1000

    fix: head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
 // import App from 'next/app'
 import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
+import { Fragment } from 'react'
 import PageLayout from '../components/Layout/PageLayout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
+      <Fragment>
+      <Head>
+        {/* Use minimum-scale=1 to enable GPU rasterization */}
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <meta name="description" content="easy way to find and download images of your property"/>
+        <meta name="keywords" content="real estate, property, technology"/>
+        <title>hiirez</title>
+      </Head>
       <PageLayout>
         <Component {...pageProps} />
       </PageLayout>
+      </Fragment>
     )
   }
   
